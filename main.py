d4d3e717fca81aaa66d3a66bcf6c17dad5f7d94e
shaammiru <msyamil.hamami@gmail.com>
2025-08-10 22:45:52 +0700

    update

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ async def recognize_face(file: UploadFile):
         rep = DeepFace.represent(
             img_path=img_array,
             model_name=MODELS[7],
-            detector_backend=BACKENDS[5],
+            detector_backend="yunet",
             normalization="ArcFace",
             enforce_detection=False,
         )
@@ -199,7 +199,7 @@ async def register_face(file: UploadFile = File(...), name: str = Form(...)):
         rep = DeepFace.represent(
             img_path=img_array,
             model_name=MODELS[7],
-            detector_backend=BACKENDS[5],
+            detector_backend="yunet",
             normalization="ArcFace",
             enforce_detection=False,
         )
